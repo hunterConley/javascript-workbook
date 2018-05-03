23c1f5015170a62eb4c30459986a6bed118424ce
unknown <hunter-conley@sbcglobal.net>
2018-05-03 18:56:27 -0500

    intial commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const printListOfOlympians = () => {
+
+    //Function accepts an array and uses a forEach
+    const olympianNames = ['Keith', 'Raul', 'Hunter'];
+    olympianNames.forEach((item, index, arr) {
+        
+    })
+};
